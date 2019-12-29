69041ea7d63d908653a9150eb215a529b26bed41
David Cantoran <davidcantoran36@gmail.com>
2019-12-29 20:22:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,4 +16,4 @@ create_project_hash
 # img_link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta li a span.location-name").text
-# percent: 
+# percent: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
